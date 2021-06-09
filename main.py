52c767bcc62e1817dc595ebd9274aebb2d2c33bd
Tobias Meinert <Tobias_meinert_vt@gmx.net>
2021-06-09 11:23:19 +0200

    deleted Picturesagain

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ from licenseplatelib import IdentifyLicensePlate as ILP
 import cv2
 import glob
 
-tFront = ILP("Bilder/tobias_front.jpeg")
-tBack = ILP("Bilder/tobias_back.jpeg")
-audiTim = ILP("Bilder/audi_tim.jpeg")
-vw_tim = ILP("Bilder/vw_tim.jpeg")
-janIstSoToll = ILP("Bilder/jansAchSoTollesBild.JPEG")
-marokko = ILP("Bilder/marokkoTaxi.jpg")
-vwAuschnitt = ILP("Bilder/ausschnitt207.jpg")
+cars = []
 
-print(glob.glob("Bilder/*.*"))
-print(tFront.get_text())
+for path in glob.glob("Bilder/*.*"):
+    cars.append(ILP(path))
+
+for car in cars:
+    print(car.path)
 #print(janIstSoToll.path + janIstSoToll.get_text())
 #print(vwAuschnitt.path + vwAuschnitt.get_text())
 
